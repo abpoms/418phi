73e641e1931060da4d562ff967332dd744695b76
Alex Poms <alexpoms@gmail.com>
2015-07-22 21:57:57 -0400

    Base for Phi helper macros

---
+++ 418Phi.h
@@ -0,0 +1,26 @@
+#ifndef 418_PHI
+#define 418_PHI
+
+// Placeholders for now
+
+/*
+  Allocates memory on the MIC device
+ */
+#define MALLOC_MIC 0;
+
+/*
+  Frees memory on the MIC device
+ */
+#define FREE_MIC 0;
+
+/*
+  Copies memory from the host to the MIC device
+ */
+#define MEMCPY_TO_MIC 0;
+
+/*
+  Copies memory from the MIC device to the host
+ */
+#define MEMCPY_TO_HOST 0;
+
+#endif
